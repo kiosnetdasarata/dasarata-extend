bed86df71ffe6c207d57317295b67d792f4ccfe2
IT Kiosnet <indra@gmail.com>
2023-05-23 09:53:57 +0700

    nomor handphone

--- resources/views/hr/employee/create.blade.php
+++ resources/views/hr/employee/create.blade.php
@@ -18,6 +18,16 @@
             @enderror
         </div>
         <div class="mb-6">
+            <label for="nama" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nomor Handphone</label>
+            <input type="text" id="no_tlpn" name="no_tlpn" class="form-control bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:focus:bg-gray-700  @error('no_tlpn') is-invalid
+            @enderror" value="{{ old('no_tlpn') }}" placeholder="Nomor Handphone" autocomplete="off">
+            @error('no_tlpn')
+            <div class="text-gray-900 invalid-feedback dark:text-white">
+                {{ $message }}
+            </div>
+            @enderror
+        </div>
+        <div class="mb-6">
             <label for="nik" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">NIK</label>
             <input name="nik" id="nik" class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 @error('nik') is-invalid
             @enderror" value="{{ old('nik') }}" type="text" placeholder="Nomor Induk Kependudukan" autocomplete="off">
